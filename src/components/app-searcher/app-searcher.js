29b6119b6d9e65cc4c2312a0d36361eb7d4f1b6b
Radik Abubjazov <abubjazov@mail.ru>
2021-10-28 02:40:58 +0300

    app-searcher: +app-searcher.css & component content

---
+++ src/components/app-searcher/app-searcher.js
@@ -0,0 +1,12 @@
+import './app-searcher.css'
+
+export const AppSearcher = () => {
+    return (
+        <div className="app-searcher">
+            <input 
+            type="text" 
+            className="form-control search-input"
+            placeholder="Найти сотрудника"/>
+        </div>
+    )
+}
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,12 @@
 import { AppInfo } from '../app-info/app-info'
+import { AppSearcher } from '../app-searcher/app-searcher'
 import './app.css'
 
 export const App = () => {
     return (
         <div className="app">
             <AppInfo />
+            <AppSearcher/>        
         </div>
     )
 }
