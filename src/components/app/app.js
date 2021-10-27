1f27a526eeee9f1fb78b5c66e7db248d8ea40987
Radik Abubjazov <abubjazov@mail.ru>
2021-10-28 02:22:41 +0300

    app-info: +app-info.css & component content

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import { AppInfo } from '../app-info/app-info'
+import './app.css'
+
+export const App = () => {
+    return (
+        <div className="app">
+            <AppInfo />
+        </div>
+    )
+}
