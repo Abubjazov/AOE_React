f2561d0c1db6eb85d94ec712c8499ae87014b4af
Radik Abubjazov <abubjazov@mail.ru>
2021-10-28 10:55:56 +0300

    app-employee: +генерация CSS по свойству like & рефакторинг через state

--- src/components/app-employee/app-employee.js
+++ src/components/app-employee/app-employee.js
@@ -1,23 +1,58 @@
+import { Component } from 'react'
 import './app-employee.css'
 
-export const AppEmployee = ({name, salary, increase}) => {
-    return (
-        <li className={increase ? `app-employee list-group-item d-flex justify-content-between increase` : `app-employee list-group-item d-flex justify-content-between`}>
-            <span className="list-group-item-label">{name}</span>
+export class AppEmployee extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            increase: props.increase,
+            like: false
+        } 
+    }
 
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
 
-            <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onEmployeeClick = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+ 
+    render() {
+        const {name, salary} = this.props
+        const {increase, like} = this.state
 
-                <button className="btn-trash btn-sm">
-                    <i className="fas fa-trash"></i>
-                </button>
+        let empClassNames = 'app-employee list-group-item d-flex justify-content-between'
 
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+        if (increase) {empClassNames += ' increase'}
+        if (like) {empClassNames += ' like'}
+
+        return (
+            <li className={empClassNames}>
+                <span 
+                    className="list-group-item-label"
+                    onClick={this.onEmployeeClick}>{name}</span>
+    
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+    
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                        className="btn-cookie btn-sm"
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button className="btn-trash btn-sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+    
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
