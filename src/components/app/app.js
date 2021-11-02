d65b2d3829885424319d5bdfc4f7176ede021088
Radik Abubjazov <abubjazov@mail.ru>
2021-11-02 17:58:49 +0300

    app-employee-addform: +упраляемый input

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,14 +4,14 @@ import { AppInfo } from '../app-info/app-info'
 import { AppSearcher } from '../app-searcher/app-searcher'
 import './app.css'
 
-export const App = () => {
-    const employeeData = [
-        {key: 1, name: 'Джон Константин', salary: 1200, increase: true},
-        {key: 2, name: 'Джон Уик', salary: 3700, increase: false},
-        {key: 3, name: 'Томас Андерсон', salary: 5000, increase: false},
-        {key: 4, name: 'Кевин Ломакс', salary: 800, increase: true}
-    ]
+const employeeData = [
+    {key: 1, name: 'Джон Константин', salary: 1200, increase: true},
+    {key: 2, name: 'Джон Уик', salary: 3700, increase: false},
+    {key: 3, name: 'Томас Андерсон', salary: 5000, increase: false},
+    {key: 4, name: 'Кевин Ломакс', salary: 800, increase: true}
+]
 
+export const App = () => {
     return (
         <div className="app">
             <AppInfo />
