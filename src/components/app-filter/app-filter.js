9fc7ca86e3b7e493a242f8ade8a081f76f036c43
Radik Abubjazov <abubjazov@mail.ru>
2021-11-04 13:00:58 +0300

    app-filter: refactor

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,40 +1,39 @@
-import { Component } from 'react'
 import './app-filter.css'
 
-export class AppFilter extends Component {
-    onFilterUpdate = (e) => {
+export const AppFilter = (props) => {
+    const onFilterUpdate = (e) => {
         const filter = e.currentTarget.getAttribute('data-filter')
 
-        this.props.onFilterUpdate(filter)
+        props.onFilterUpdate(filter)
         e.target.parentNode.childNodes.forEach(elem =>  elem.className = 'btn btn-outline-light')
         e.target.className = 'btn btn-light'
     }
 
-    render() {
-        const buttonsArr = [
-            {name: 'all', label: 'Все сотрудники'},
-            {name: 'liked', label: 'На повышение'},
-            {name: '1000', label: 'З/П более 1000$'}
-        ]
 
-        const buttons = buttonsArr.map(({name, label}) => {
-            return (
-                <button 
-                    className={name === 'all' ? 'btn btn-light' : 'btn btn-outline-light'}
-                    type="button"
-                    data-filter={name}
-                    onClick={this.onFilterUpdate}>
-                        {label}
-                </button>
-            )
-        })
+    const buttonsArr = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'liked', label: 'На повышение'},
+        {name: '1000', label: 'З/П более 1000$'}
+    ]
 
+    const buttons = buttonsArr.map(({name, label}) => {
         return (
-            <div className="app-filter">
-                <div className="btn-group">
-                    {buttons}
-                </div>
-            </div>
+            <button 
+                className={name === 'all' ? 'btn btn-light' : 'btn btn-outline-light'}
+                type="button"
+                key={name}
+                data-filter={name}
+                onClick={onFilterUpdate}>
+                    {label}
+            </button>
         )
-    }
+    })
+
+    return (
+        <div className="app-filter">
+            <div className="btn-group">
+                {buttons}
+            </div>
+        </div>
+    )
 }
